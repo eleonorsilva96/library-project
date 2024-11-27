8df6c0ca7034b1588f52ab1c4ee79f71ed20f4c1
Eleonor Silva <eleonorsilva@gmail.com>
2024-11-27 19:30:38 +0000

    add class constructor and improve UI

--- script.js
+++ script.js
@@ -6,102 +6,219 @@ const table = document.querySelector("table");
 const dialog = document.querySelector("#dialog");
 const showDialog = document.querySelector("#showDialog");
 const confirmBtn = document.querySelector("button#confirmBtn");
+const readList = document.querySelector("#read-list > ul");
+//my library
+// const book = document.querySelector(".library > .book");
+const library = document.querySelector(".library");
+const book = `
+            <div class="book">
+                    <div class="title-author">
+                        <i class="fa-solid fa-book"></i>
+                        <h3 id="title"></h3>
+                        <div class="author-pages">
+                            <h4 id="author"></h4> - <div id="pages"></div>
+                        </div>
+                    </div>
+                    <div class="read-button">
+                        <button id="read-book">Read</button>
+                    </div>
+                    <div class="remove-button">
+                        <button id="remove-book">Remove</button>
+                    </div>
+                </div>
+`;
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
 
 //event listeners
 showDialog.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 confirmBtn.addEventListener("click", (e) => {
-    e.preventDefault(); //it will not submit the form
-    
-    let authorValue = document.querySelector("input#author").value;
-    let titleValue = document.querySelector("input#title").value;
-    let pagesValue = document.querySelector("input#pages").value;
-    
-    if (authorValue && titleValue && pagesValue) {
-        verifyDuplicatedBooks(authorValue, titleValue);
+  e.preventDefault(); //it will not submit the form
 
-        if (isMatching === true) {
-            //if has found a matching input value dont create a new object book
-            isMatching = false;
-        } else {
-            //create a new object instance and push it to the library array
-            const newBook = new Book(authorValue, titleValue, pagesValue);
-            myLibrary.push(newBook);
+  let authorValue = document.querySelector("input#author").value;
+  let titleValue = document.querySelector("input#title").value;
+  let pagesValue = document.querySelector("input#pages").value;
 
-            showNewBook(newBook);
-        }
+  if (authorValue && titleValue && pagesValue) {
+    verifyDuplicatedBooks(authorValue, titleValue);
+
+    if (isMatching === true) {
+      //if has found a matching input value dont create a new object book
+      isMatching = false;
+    } else {
+      //create a new object instance and push it to the library array
+      const newBook = new Book(authorValue, titleValue, pagesValue);
+      myLibrary.push(newBook);
+
+      showNewBook(newBook);
     }
-    
-    dialog.close();
+  }
+
+  dialog.close();
 });
 
 // object constructor
-function Book(author, title, numberPages) {
-  this.id = Math.random().toString(36).substring(2, 9);
-  this.author = author;
-  this.title = title;
-  this.numberPages = numberPages;
+class Book {
+  constructor(author, title, numberPages) {
+    this.id = Math.random().toString(36).substring(2, 9);
+    this.author = author;
+    this.title = title;
+    this.numberPages = numberPages;
+  }
+
+  changeStatus(checkbox) {
+    if (checkbox.checked) {
+      // this.status = status;
+      const readItem = document.createElement("li");
+      readList.appendChild(readItem);
+
+      readItem.textContent = `Read ${this.author}`;
+    } else {
+      // this.status = false;
+      const readItem = document.querySelector("#read-list > ul > li");
+
+      readItem.remove();
+    }
+  }
 }
 
-Book.prototype.changeStatus = function (statusCheckbox) {
-  const readList = document.querySelector("#read-list > ul");
+function addBookToLibrary(author, title, numberPages) {
+  const objectBook = new Book(author, title, numberPages);
+  // const bookId = objectBook.id;
 
-  if (statusCheckbox.checked) {
-    this.status = true;
+  myLibrary.push(objectBook);
 
-    const readItem = document.createElement("li");
-    readList.appendChild(readItem);
+  //create a table row and pass the index of the added object book
+  showBookFromLibrary();
+}
 
-    readItem.textContent = `Read ${this.author}`;
+function showBookFromLibrary() {
+  //  book.setAttribute("data-id", id);
+  let bookContent = "";
+
+  myLibrary.forEach((book) => {
+    bookContent += `
+            <div class="book" data-id="${book.id}">
+                    <div class="title-author">
+                        <img src="book-solid.svg" alt="book icon" width="28px" height="28px">
+                        <h3 id="title">${book.title}</h3>
+                        <div class="author-pages">
+                            <h4 id="author">${book.author}</h4> ‧ <div class="pages" id="pages">${book.numberPages} pages</div>
+                        </div>
+                    </div>
+                      <div class="book-buttons">
+                      <div class="read-button">
+                          <button id="read-book">Read</button>
+                      </div>
+                      <div class="remove-button">
+                          <button id="remove-book">Remove</button>
+                      </div>
+                    </div>
+                </div>
+                `;
+  });
+
+  library.innerHTML = bookContent;
+
+  // const tableCellButton = document.createElement("td");
+  // const removeButton = document.createElement("button");
+  // tableCellButton.appendChild(removeButton);
+
+  // const tableCellCheck = document.createElement("td");
+  // const statusCheck = document.createElement("input");
+  // statusCheck.setAttribute("type", "checkbox");
+  // tableCellCheck.appendChild(statusCheck);
+
+  //the values method converts the book objects values in an array ["author", "title", "numberPages"]
+  // Object.values(book).forEach((detail) => {
+  //     const tableCell = document.createElement("td");
+  //     tableCell.textContent = detail;
+
+  //     tableRow.appendChild(tableCell);
+  // });
+
+  // tableRow.appendChild(tableCellButton);
+  // tableRow.appendChild(tableCellCheck);
+
+  // table.appendChild(tableRow);
+
+  //add event handlers
+  // removeButton.addEventListener("click", () => removeBook(id));
+  // statusCheck.addEventListener("change", () => book.changeStatus(statusCheck));
+}
+
+function removeBook(id) {
+  //find the position of the book in the array with the book object's id
+  console.log(myLibrary);
+  console.log(id);
+  const index = myLibrary.findIndex((book) => book.id === id);
+  console.log(index);
+  //verify if its not null
+  if (index !== -1) {
+    console.log("remove");
+    myLibrary.splice(index, 1);
+    const tableRow = document.querySelector(`tr[data-id='${id}'`);
+
+    if (tableRow) {
+      tableRow.remove();
+    } else {
+      console.error("book row not found");
+    }
   } else {
-    this.status = false;
-
-    const readItem = document.querySelector("#read-list > ul > li");
-    readItem.remove();
+    console.error("book not found");
   }
-};
+}
 
+function verifyDuplicatedBooks(newAuthor, newTitle) {
+  myLibrary.forEach((book) => {
+    if (newAuthor === book.author && newTitle === book.title) {
+      isMatching = true;
 
+      console.log("matching!");
+    }
+  });
+}
 
+addBookToLibrary("Sara Gruen", "Water for Elephants", "300");
+addBookToLibrary("R.F. Kuang", "The Burning God (The Poppy War, #3)", "362");
+addBookToLibrary("Haruki Murakami", "Sputnik Sweetheart", "200");
+addBookToLibrary("Sara Gruen", "Water for Elephants", "300");
+addBookToLibrary("R.F. Kuang", "The Burning God (The Poppy War, #3)", "362");
+addBookToLibrary("Haruki Murakami", "Sputnik Sweetheart", "200");
+
+// function Book(author, title, numberPages) {
+//   this.id = Math.random().toString(36).substring(2, 9);
+//   this.author = author;
+//   this.title = title;
+//   this.numberPages = numberPages;
+// }
+
+// Book.prototype.changeStatus = function (statusCheckbox) {
+//   const readList = document.querySelector("#read-list > ul");
+
+//   if (statusCheckbox.checked) {
+//     this.status = true;
+
+//     const readItem = document.createElement("li");
+//     readList.appendChild(readItem);
+
+//     readItem.textContent = `Read ${this.author}`;
+//   } else {
+//     this.status = false;
+
+//     const readItem = document.querySelector("#read-list > ul > li");
+//     readItem.remove();
+//   }
+// };
 
 // function addBookToLibrary(author, title, numberPages) {
 //   const newBook = new Book(author, title, numberPages);
 //   myLibrary.push(newBook);
 // }
-function createTableRow(book, id) {
-     const tableRow = document.createElement("tr");
-      tableRow.setAttribute("data-id", id);
-
-      const tableCellButton = document.createElement("td");
-      const removeButton = document.createElement("button");
-      tableCellButton.appendChild(removeButton);
-
-      const tableCellCheck = document.createElement("td");
-      const statusCheck = document.createElement("input");
-      statusCheck.setAttribute("type", "checkbox");
-      tableCellCheck.appendChild(statusCheck);
-  
-    //the values method converts the book objects values in an array ["author", "title", "numberPages"]
-    Object.values(book).forEach((detail) => {
-        const tableCell = document.createElement("td");
-        tableCell.textContent = detail;
-        
-        tableRow.appendChild(tableCell);
-    });
-    
-    tableRow.appendChild(tableCellButton);
-    tableRow.appendChild(tableCellCheck);
-
-    table.appendChild(tableRow);
-
-    //add event handlers
-    removeButton.addEventListener("click", () => removeBook(id));
-    statusCheck.addEventListener("change", () => book.changeStatus(statusCheck));
-    
-}
-
 
 // function updateBooks() {
 //     let newIndex = 0;
@@ -111,10 +228,10 @@ function createTableRow(book, id) {
 // //     for(let i = 0; i < tableRows; i++) {
 // //         // createTableRow(book, index);
 // //         const tableRowUpdate = document.querySelector(`tr[data-index='${i}']`);
-        
+
 // //         console.log(i);
 // //         // console.log(tableRowUpdate);
-    
+
 // //         if(tableRowUpdate) {
 // //         tableRowUpdate.setAttribute("data-index", index);
 // //         index++;
@@ -123,7 +240,7 @@ function createTableRow(book, id) {
 
 //   tableRows.forEach((row, index) => {
 //     const tableRowUpdate = document.querySelector(`tr[data-index='${index}']`);
-        
+
 //     console.log(index);
 //     // console.log(tableRowUpdate);
 //     if(tableRowUpdate) {
@@ -132,7 +249,6 @@ function createTableRow(book, id) {
 // }
 // // console.log(tableRows.length);
 
-
 // });
 
 // if(document.querySelector(`tr[data-index='${tableRows.length}']`)) {
@@ -140,39 +256,6 @@ function createTableRow(book, id) {
 //     document.querySelector(`tr[data-index='${tableRows.length}']`).setAttribute("data-index", tableRows.length - 1);
 // }
 // }
-
-function addBookToLibrary(author, title, numberPages) {
-  const objectBook = new Book(author, title, numberPages);
-
-  myLibrary.push(objectBook);
-
-  const bookId = objectBook.id;
-  
-  //create a table row and pass the index of the added object book
-  createTableRow(objectBook, bookId);
-}
-
-function removeBook(id) {
-    //find the position of the book in the array with the book object's id 
-    console.log(myLibrary);
-    console.log(id);
-    const index = myLibrary.findIndex((book) => book.id === id)
-    console.log(index);
-    //verify if its not null
-    if(index !== -1) {
-        console.log("remove");
-        myLibrary.splice(index, 1);
-        const tableRow = document.querySelector(`tr[data-id='${id}'`);
-
-        if(tableRow) {
-            tableRow.remove();
-        } else {
-            console.error("book row not found")
-        }
-    } else {
-        console.error("book not found");
-    }
-}
 
 // function setActionsOnNewBooks(newEntryIndex, newObjectInstance) {
 //     const removeButton = document.querySelector(
@@ -194,16 +277,6 @@ function removeBook(id) {
 //   }
 // }
 
-function verifyDuplicatedBooks(newAuthor, newTitle) {
-  myLibrary.forEach((book) => {
-    if (newAuthor === book.author && newTitle === book.title) {
-      isMatching = true;
-
-      console.log("matching!");
-    }
-  });
-}
-
 // function setActionsOnListedBooks() {
 //     myLibrary.forEach((book, index) => {
 //       console.log(index);
@@ -214,13 +287,13 @@ function verifyDuplicatedBooks(newAuthor, newTitle) {
 //           const statusCheckbox = document.querySelector(
 //             `tr[data-index='${index}'] > td > input[type=checkbox]`
 //           );
-    
+
 //           if (removeButton && statusCheckbox) {
 //             removeButton.addEventListener("click", () => {
 //               removeBook(index);
 //               console.log("clicked remove button!");
 //             });
-    
+
 //             statusCheckbox.addEventListener("change", () => {
 //                 if(index === 1) {
 //                     book1.changeStatus(statusCheckbox);
@@ -232,13 +305,10 @@ function verifyDuplicatedBooks(newAuthor, newTitle) {
 //             });
 //       }
 
-
 //     });
 
 //   // console.log(myLibrary);
 // }
-
-
 
 //add some books objects
 // const book1 = new Book("Sara Gruen", "Water for Elephants", "300");
@@ -254,17 +324,7 @@ function verifyDuplicatedBooks(newAuthor, newTitle) {
 
 // book1.changeStatus();
 
-
 // Modal
-
-
-
-addBookToLibrary("Sara Gruen", "Water for Elephants", "300");
-addBookToLibrary("R.F. Kuang", "The Burning God (The Poppy War, #3)", "362");
-addBookToLibrary("Haruki Murakami", "Sputnik Sweetheart", "200");
-addBookToLibrary("Sara Gruen", "Water for Elephants", "300");
-addBookToLibrary("R.F. Kuang", "The Burning God (The Poppy War, #3)", "362");
-addBookToLibrary("Haruki Murakami", "Sputnik Sweetheart", "200");
 
 // listAllBooksAndActions();
 // removeButtonDOM.addEventListener("click", removeBook());
